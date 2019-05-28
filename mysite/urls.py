18f39eb51749482a341738ea8a7abdcb8da40bac
Цырендылыкова Эржена <tsyrendylykova@Cyrendylykovas-MacBook-Pro.local>
2019-05-28 10:32:24 +0300

    [add] image

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trainer.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += patterns('', 
+# (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+# {'document_root': settings.MEDIA_ROOT}), 
+# )
